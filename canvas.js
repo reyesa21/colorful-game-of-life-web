8c55f36a6053b006a1ac86a93d80b4897ed31697
reyesa21 <71284638+reyesa21@users.noreply.github.com>
2021-06-05 22:24:18 -0700

    functional game of life

--- canvas.js
+++ canvas.js
@@ -1,25 +1,32 @@
 
-var canvas = document.getElementById('draw');
-var life = Array(w);
-var prevLife = Array(w);
+'use strict';
 
+var canvas = document.getElementById('draw');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const SCALE = 2;
+const PSIZE = 10 * SCALE;
+
+var width = Math.trunc(canvas.width / PSIZE);
+var height = Math.trunc(canvas.height / PSIZE);
+var life = Array(width);
+var prevLife = Array(width);
+
+
 var mouseDown = false;
 
-var w = canvas.width;
-var h = canvas.height;
 var ctx = canvas.getContext('2d');
-
 window.onload = () => {
-  for (var i = 0; i < w; i++) {
-    life[i] = Array(h).fill(false);
-    prevLife[i] = Array(h).fill(false);
+
+
+  for (var i = 0; i < width; i++) {
+    life[i] = Array(height).fill(false);
+    prevLife[i] = Array(height).fill(false);
   }
 
   for (let i = 0; i < 100; i++) {
-    let randomI = getRandomInt(canvas.width);
-    let randomJ = getRandomInt(canvas.height);
+    let randomI = getRandomInt(width);
+    let randomJ = getRandomInt(height);
     life[randomI][randomJ] = true;
   }
 
@@ -27,8 +34,8 @@ window.onload = () => {
 };
 
 function refreshLife() {
-  for (let i = 0; i < canvas.width; i++)
-    for (let j = 0; j < canvas.height; j++) {
+  for (let i = 0; i < width; i++)
+    for (let j = 0; j < height; j++) {
       if (life[i][j] != prevLife[i][j])
         updateState(i, j);
     }
@@ -39,42 +46,69 @@ function getRandomInt(max) {
 }
 
 function updateState(i, j) {
-  if (i < w && j < h) {
+  if (i < width && j < height) {
     if (life[i][j] == true){
-      ctx.fillStyle = "#000";
+      ctx.fillStyle = getColors();
     }
-    else{
+    else
       ctx.fillStyle = "#FFF";
-      console.log('no');
-    }
 
-    ctx.fillRect(Math.trunc(i / 10) * 10, Math.trunc(j / 10) * 10, 10, 10);
+    ctx.fillRect(i * PSIZE, j * PSIZE, PSIZE, PSIZE);
     
   }
+}
+
+function getColors(){
+  let random = getRandomInt(10);
+  switch (random)
+  {
+      case 0:
+          return "#F10891";
+      case 1:
+          return "#B00B69";
+      case 2:
+          return "#FAFAFA";
+      case 3:
+          return "#FEEEEE";
+      case 4:
+          return "#FEDCAA";
+      case 5:
+          return "#223445";
+      case 6:
+          return "#323231";
+      case 7:
+          return "#311222"
+      case 8:
+          return "#666666";     
+        case 9:
+          return "#555555";
+  }
+
+  return "#fff222";
 }
 
 function neighbors(i, j) {
   let nb = 0;
   if (j - 1 >= 0) {
-    if (prevLife[i, j - 1] == true) {
+    if (prevLife[i][j - 1] == true) {
       nb++;
     }
   }
-  if (j + 1 < h) {
-    if (prevLife[i, j + 1] == true) {
+  if (j + 1 < height) {
+    if (prevLife[i][j + 1] == true) {
       nb++;
     }
   }
   for (let k = 0; k < 3; k++) {
-    if (i - 1 >= 0 && j - 1 + k >= 0 && i - 1 < w && j - 1 + k < h) {
-      if (prevLife[i - 1, j - 1 + k] == true) {
+    if (i - 1 >= 0 && j - 1 + k >= 0 && i - 1 < width && j - 1 + k < height) {
+      if (prevLife[i - 1][j - 1 + k] == true) {
         nb++;
       }
     }
   }
   for (let k = 0; k < 3; k++) {
-    if (i + 1 >= 0 && j - 1 + k >= 0 && i + 1 < w && j - 1 + k < h) {
-      if (prevLife[i + 1, j - 1 + k] == true) {
+    if (i + 1 >= 0 && j - 1 + k >= 0 && i + 1 < width && j - 1 + k < height) {
+      if (prevLife[i + 1][j - 1 + k] == true) {
         nb++;
       }
     }
@@ -83,38 +117,46 @@ function neighbors(i, j) {
 }
 
 function live() {
+  for (let i = 0; i < width; i++)
+  for (let j = 0; j < height; j++)
+    prevLife[i][j] = life[i][j];
+
   let nb = 0;
-
-
-  for (i = 0; i < w; i++)
-    for (j = 0; j < h; j++)
-      prevLife[i, j] = life[i, j];
-
-  for (let a = 0; a < w; a++) {
-    for (let b = 0; b < h; b++) {
+  console.log('1');
+  for (let a = 0; a < width; a++) {
+    for (let b = 0; b < height; b++) {
       nb = neighbors(a, b);
-      if (nb != 2) {
-        life[a, b] = (nb == 3);
-
-      }
+      console.log(nb);
+      life[a][b] = nb != 2 ? nb == 3 : life[a][b];
     }
   }
+
   refreshLife();
+  if(!mouseDown)
+    setTimeout(live, 100);
 }
 
 canvas.addEventListener('mousedown', e => {
+  ctx.fillStyle = getColors();
+
   mouseDown = true;
-  ctx.fillRect(Math.trunc(e.offsetX / 10) * 10, Math.trunc(e.offsetY / 10) * 10, 10, 10);
-  life[Math.trunc(e.offsetX / 10) * 10, Math.trunc(e.offsetY / 10)];
+  let x = Math.trunc(e.offsetX / PSIZE);
+  let y = Math.trunc(e.offsetY / PSIZE);
+  ctx.fillRect(x * PSIZE, y * PSIZE, PSIZE, PSIZE);
+  life[x][y] = true;
 });
+
 
 canvas.addEventListener('mousemove', e => {
   if (mouseDown) {
-    ctx.fillRect(Math.trunc(e.offsetX / 10) * 10, Math.trunc(e.offsetY / 10) * 10, 10, 10);
-    life[Math.trunc(e.offsetX / 10) * 10, Math.trunc(e.offsetY / 10)];
+    let x = Math.trunc(e.offsetX / PSIZE);
+    let y = Math.trunc(e.offsetY / PSIZE);
+    ctx.fillRect(x * PSIZE, y * PSIZE, PSIZE, PSIZE);
+    life[x][y] = true;
   }
 });
 canvas.addEventListener('mouseup', e => {
   mouseDown = false;
-  refreshLife();
+  live();  
 });
+
